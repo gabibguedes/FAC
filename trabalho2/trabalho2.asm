066479f879c35e794c814ddc0b4cb9cbf4307a07
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-04-26 11:57:15 -0300

    Adicionando chamadas das outras funções e e imprimindo o resultado
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- trabalho2/trabalho2.asm
+++ trabalho2/trabalho2.asm
@@ -5,7 +5,12 @@
   	expoente: .asciiz "expoente: "
   	modulo: .asciiz "modulo: "
   	primo: .asciiz "eh primo"
-  	erro: .asciiz "Nao eh primo"
+  	erro: .asciiz "O modulo nao eh primo"
+  	saida1: .asciiz "A exponencial modular "
+  	saida2: .asciiz " elevado a "
+  	saida3: .asciiz " (mod "
+  	saida4: .asciiz ") eh "
+  	saida5: .ascii "."
 .text
 
 j MAIN
@@ -15,13 +20,7 @@ le_inteiro:
 	syscall
 	
 	move $t0, $v0
-	
-	li $v0, 1
-	move $a0, $t0
-	syscall
-
 	jr $ra
-	
 
 eh_primo: 
 	add $t5, $0, $0 #Variavel para contar quantas divisôes tem o número
@@ -35,12 +34,6 @@ eh_primo:
 		
 	IF2:
 		li $s0,  1 #Coloca 1 na variavel $s0 se for primo
-		
-		##Imprime "EH PRIMO"
-		li $v0, 4
-		la $a0, primo
-		syscall
-		
 		j volta_do_if2 #volta
 		
 	FOR:	
@@ -55,13 +48,55 @@ eh_primo:
 	beq  $t5, 2, IF2 # Se tiver 2 divisores entra no if2
 	volta_do_if2:
 		jr $ra #volta pra main
+	
+calc_exp:
+	jr $ra
 
-		
-nao_primo:
+imprime_erro:
 	li $v0, 4
 	la $a0, erro
 	syscall
-	j END			
+	j END	
+
+imprime_saida:
+	li $v0, 4
+	la $a0, saida1
+	syscall
+	
+	li $v0, 1
+	move $a0, $t1
+	syscall # Imprime a BASE
+	
+	li $v0, 4
+	la $a0, saida2
+	syscall
+	
+	li $v0, 1
+	move $a0, $t2
+	syscall # Imprime O EXPONECIAL
+	
+	li $v0, 4
+	la $a0, saida3
+	syscall
+	
+	li $v0, 1
+	move $a0, $t3
+	syscall # Imprime o MODULO
+	
+	li $v0, 4
+	la $a0, saida4
+	syscall
+	
+	li $v0, 1
+	move $a0, $s1
+	syscall # Imprime o RESULTADO
+	
+	li $v0, 4
+	la $a0, saida5
+	syscall
+	
+	jr $ra
+	
 
 
 MAIN:
@@ -91,7 +126,10 @@ MAIN:
 	
 	jal eh_primo
 	
-	beqz $s0, nao_primo
+	beqz $s0, imprime_erro
+	
+	jal calc_exp
+	jal imprime_saida
 		
 	addi $t4, $0, 0X0000CAFE
 	
